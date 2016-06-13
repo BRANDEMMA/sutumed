8919c3b9da9fd57343e91048f5e96e431077edd7
JFRZ7 <jose.francisco.romero.7@gmail.com>
2016-06-13 16:29:00 -0500

    enlace de producto

--- productos-suturas-absorbibles.php
+++ productos-suturas-absorbibles.php
@@ -5,7 +5,7 @@
 		<p>Las suturas quirúrgicas se emplean para aproximar tejidos y ligar vasos sanguíneos de forma temporal, y se aplican en diversos procedimientos de acuerdo a las propiedades y características de la hebra y aguja con que han sido fabricadas. Pueden ser degradadas por enzimas (en el caso de la sutura de origen natural de Catgut) o hidrolizadas por fluidos tisulares (en el caso de las sintéticas), por lo que no es necesario retirarlas. Su proceso de degradación hace que la resistencia de estas suturas disminuya con el tiempo.</p>
 
 		<div class="nuevos-productos">
-			<a href=""><img src="img/producto-sutures.jpg"></a>
+			<a href="producto.php"><img src="img/producto-sutures.jpg"></a>
 			<a href=""><h4>Antibacterial Polyglycolic Acid</h4>
 			<p>Suture fast absorption</p></a>
 		</div>
